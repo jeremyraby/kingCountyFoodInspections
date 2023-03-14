4d7782061a0edb0ff9a21f9f4ebb3d7e4372345d
jeremyraby <jeremy.raby10@gmail.com>
2023-03-13 19:27:18 -0500

    Add 10 most/least written violations

--- foodInspection.sql
+++ foodInspection.sql
@@ -84,6 +84,106 @@ ORDER BY 2 DESC
 LIMIT 1;
 -- (Restaurant) TASTY SZECHUAN	(AVG score) 84.3913043478261
 
+-- What are the 5 most common violations
+
+SELECT 
+    violation_num,
+    violation_description,
+    COUNT(violation_num) AS 'count'
+FROM reports
+WHERE violation_num != ''
+GROUP BY 1
+ORDER BY 3 DESC
+LIMIT 5;
+/*
+violation_num, violation_description, count
+3400, Wiping cloths properly used, stored, proper sanitizer, 12288
+2110, Proper cold holding temperatures (greater than  45 degrees F), 12030
+600, Adequate handwashing facilities, 11858
+200, Food Worker Cards current for all food workers; new food workers trained, 8621
+4100, Warewashing facilities properly installed, 8126
+*/
+
+-- Did 98105 Zip Code have a change in number of red violations
+-- since new inspector was hired (between 2017 & 2019)?
+
+SELECT DISTINCT(COUNT(violation_type))
+FROM reports
+WHERE 
+    inspection_date LIKE '%/17' AND
+    zip_code = '98105' AND
+    violation_type LIKE 'red';
+-- 2017: 316
+
+SELECT DISTINCT(COUNT(violation_type))
+FROM reports
+WHERE 
+    inspection_date LIKE '%/19' AND
+    zip_code = '98105' AND
+    violation_type LIKE 'red';
+-- 2019: 144
+
+-- percent change 
+SELECT ROUND(((144.0-316)/316) * 100, 2);
+-- -54.43%
+-- "Red" violations in zip code 98105 decreased by 54.43% between 2017 & 2019
+
+-- Which are the 10 violations sans are writing most/least?
+-- use zip code as proxy
+-- are these vios being written properly?
+
+-- MOST
+SELECT 
+   violation_num, 
+   violation_type,
+   COUNT(violation_num)
+FROM reports
+WHERE 
+    inspection_date LIKE '%/19' AND
+    zip_code = '98105' AND
+    violation_num != ''
+GROUP BY 1
+ORDER BY 3 DESC
+LIMIT 10;
+/*
+violation_num, violation_type, COUNT(violation_num)
+3400, BLUE, 36
+2110, RED, 26
+4100, BLUE, 25
+4200, BLUE, 21
+600, RED, 20
+3300, BLUE, 20
+200, RED, 17
+1400, RED, 16
+2200, RED, 14
+3700, BLUE, 12
+*/
+-- LEAST
+SELECT 
+   violation_num, 
+   violation_type,
+   COUNT(violation_num)
+FROM reports
+WHERE 
+    inspection_date LIKE '%/19' AND
+    zip_code = '98105' AND
+    violation_num != ''
+GROUP BY 1
+ORDER BY 3
+LIMIT 10;
+/*
+violation_num, violation_type, COUNT(violation_num)
+1000, RED, 1
+1710, RED, 1
+2300, RED, 1
+2600, RED, 1
+2800, BLUE, 1
+3100, BLUE, 1
+4500, BLUE, 1
+1500, RED, 2
+1720, RED, 2
+2900, BLUE, 2
+*/
 /*
 Per Josh:
   - Care more about what is the san doing on inspections vs what violations are observed -> no info
